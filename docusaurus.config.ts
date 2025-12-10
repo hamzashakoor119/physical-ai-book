16860170647e8513f9d84d5129c0284e0aeba9ff
hamzashakoor119 <135787611+hamzashakoor119@users.noreply.github.com>
2025-12-11 01:13:34 +0500

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
       title: 'Physical AI Book',
       logo: {
         alt: 'Physical AI Logo',
-        src: 'img/logo.svg',
+        src: 'images/logo.jpg',
       },
       items: [
         {
